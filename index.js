171707c7c523959175008b73ebcd971155ce9994
RanjithKumar Sekar <kumar@kssmart.co>
2024-10-09 20:13:54 +0530

    cors removed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {cors: {origin:"*"}});
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 let userLocations = {};
