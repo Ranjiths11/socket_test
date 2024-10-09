80190133800ae42b8b9eecde6c3410ad8e4f6ca0
RanjithKumar Sekar <kumar@kssmart.co>
2024-10-09 16:22:25 +0530

    cors added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO with CORS options
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000", // Allow requests from your Next.js app
+        origin: "https://awhere-93a4f.web.app", // Allow requests from your Next.js app
         methods: ["GET", "POST"],
     },
 });
