d46179e94875120dec6609d8c2a9072279aa593f
RanjithKumar Sekar <kumar@kssmart.co>
2024-10-24 23:19:23 +0530

    done

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# socket_test
+# AWHERE PROJECT
+# socket Version ^4.8.0
+# Getting this from socket for realtime communication 
+# latitude longitude, speed, heading ,altitude,name,group
--- index.js
+++ index.js
@@ -35,7 +35,9 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 5000;
+const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
